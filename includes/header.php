d2ad72661f744a2294da01e83a7ff1267b2b4fe0
İLKİN HEYDAROV <80514053+ilkin-heydarov@users.noreply.github.com>
2025-03-03 01:57:31 +0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<header class="bg-dark text-white py-3">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg navbar-dark">
+                    <a class="navbar-brand" href="index.php">Haber Portalı</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="index.php">Ana Sayfa</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=general">Genel</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=business">Ekonomi</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=technology">Teknoloji</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=sports">Spor</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=entertainment">Magazin</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="kategori.php?cat=science">Bilim</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header> 
