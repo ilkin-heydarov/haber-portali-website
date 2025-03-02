f419f936223e6aee207e5c6c599d02e0b8fed1c6
İLKİN HEYDAROV <80514053+ilkin-heydarov@users.noreply.github.com>
2025-03-03 01:57:05 +0300

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer class="bg-dark text-white py-4 mt-5">
+    <div class="container">
+        <div class="row">
+           
+            
+        </div>
+        <div class="row mt-3">
+            <div class="col-12 text-center">
+                <p class="mb-0">&copy; <?php echo date('Y'); ?> Haber Portalı Websitesi. <br>Designed by  <a href="https://www.ilkinheydarov.com.tr/" style="text-decoration: none; color: #f1f1f1" target="_blank">Mr İlkin</a></p>
+            </div>
+        </div>
+    </div>
+</footer> 
